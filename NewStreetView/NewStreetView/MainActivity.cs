3890ec1dbaf091f73551319c53f3dc20fc77de65
Paul Johnson <paul@all-the-johnsons.co.uk>
2024-06-10 12:52:54 +0100

    Add files via upload

---
+++ NewStreetView/NewStreetView/MainActivity.cs
@@ -0,0 +1,13 @@
+namespace NewStreetView
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            StartActivity(typeof(PanoramaViewActivity));
+        }
+    }
+}
---
+++ NewStreetView/NewStreetView/PanoramaViewActivity.cs
@@ -0,0 +1,89 @@
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+
+namespace NewStreetView;
+
+[Activity(Label = "PanoramaViewActivity")]
+public class PanoramaViewActivity : AppCompatActivity, IOnStreetViewPanoramaReadyCallback
+{
+		LatLng latlng;
+    StreetViewPanoramaView streetViewPanoramaView;
+    StreetViewPanorama streetPanorama;
+
+    protected override void OnCreate(Bundle bundle)
+    {
+        base.OnCreate(bundle);
+        SetContentView(Resource.Layout.panorama);
+        var lat = Intent.GetDoubleExtra("lat", 37.7977);
+        var lng = Intent.GetDoubleExtra("lng", -122.40);
+
+        latlng = new LatLng(lat, lng);
+        streetViewPanoramaView = FindViewById<StreetViewPanoramaView>(Resource.Id.panorama);
+        streetViewPanoramaView.OnCreate(bundle);
+        streetViewPanoramaView.GetStreetViewPanoramaAsync(this);
+
+#if !DEBUG
+			Xamarin.Insights.Track ("AppNav", new Dictionary<string,string> {
+				{ "page", "panorama" }
+			});
+#endif
+    }
+
+    public override void OnWindowFocusChanged(bool hasFocus)
+    {
+        base.OnWindowFocusChanged(hasFocus);
+        if (hasFocus && (int)Build.VERSION.SdkInt >= 19)
+        {
+            Window.DecorView.SystemUiVisibility =
+                (StatusBarVisibility)(SystemUiFlags.LayoutStable
+                    | SystemUiFlags.HideNavigation
+                    | SystemUiFlags.LayoutFullscreen
+                    | SystemUiFlags.Fullscreen
+                    | SystemUiFlags.ImmersiveSticky);
+        }
+    }
+
+    public void OnStreetViewPanoramaReady(StreetViewPanorama panorama)
+    {
+        streetPanorama = panorama;
+        streetPanorama.UserNavigationEnabled = true;
+        streetPanorama.StreetNamesEnabled = true;
+        streetPanorama.PanningGesturesEnabled = true;
+        streetPanorama.ZoomGesturesEnabled = true;
+
+        streetPanorama.SetPosition(latlng);
+    }
+
+    protected override void OnResume()
+    {
+        base.OnResume();
+        streetViewPanoramaView.OnResume();
+    }
+
+    protected override void OnPause()
+    {
+        base.OnPause();
+        streetViewPanoramaView.OnPause();
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        streetViewPanoramaView.OnDestroy();
+    }
+
+    public override void OnLowMemory()
+    {
+        base.OnLowMemory();
+        streetViewPanoramaView.OnLowMemory();
+    }
+
+    protected override void OnSaveInstanceState(Bundle outState)
+    {
+        base.OnSaveInstanceState(outState);
+        streetViewPanoramaView.OnSaveInstanceState(outState);
+    }
+}
